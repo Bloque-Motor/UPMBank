784768ac126c8b31554f73045e769b8e7bd05df6
Saabertooth1 <nalas3@icloud.com>
2020-06-03 16:40:32 +0200

    Generated code from wsdl

---
+++ src/es/upm/fi/sos/upmbank/UPMBankWSSkeleton.java
@@ -0,0 +1,139 @@
+/**
+ * UPMBankWSSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.9  Built on : Nov 16, 2018 (12:05:37 GMT)
+ */
+package es.upm.fi.sos.upmbank;
+
+/**
+ *  UPMBankWSSkeleton java skeleton for the axisService
+ */
+public class UPMBankWSSkeleton {
+    /**
+     * Auto generated method signature
+     *
+     * @param addBankAcc
+     * @return addBankAccResponse
+     */
+    public es.upm.fi.sos.upmbank.AddBankAccResponse addBankAcc(
+        es.upm.fi.sos.upmbank.AddBankAcc addBankAcc) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#addBankAcc");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param closeBankAcc
+     * @return closeBankAccResponse
+     */
+    public es.upm.fi.sos.upmbank.CloseBankAccResponse closeBankAcc(
+        es.upm.fi.sos.upmbank.CloseBankAcc closeBankAcc) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#closeBankAcc");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param logout
+     * @return
+     */
+    public void logout(es.upm.fi.sos.upmbank.Logout logout) {
+        //TODO : fill this with the necessary business logic
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param removeUser
+     * @return removeUserResponse
+     */
+    public es.upm.fi.sos.upmbank.RemoveUserResponse removeUser(
+        es.upm.fi.sos.upmbank.RemoveUser removeUser) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#removeUser");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addWithdrawal
+     * @return addWithdrawalResponse
+     */
+    public es.upm.fi.sos.upmbank.AddWithdrawalResponse addWithdrawal(
+        es.upm.fi.sos.upmbank.AddWithdrawal addWithdrawal) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#addWithdrawal");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addUser
+     * @return addUserResponse
+     */
+    public es.upm.fi.sos.upmbank.AddUserResponse addUser(
+        es.upm.fi.sos.upmbank.AddUser addUser) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#addUser");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param addIncome
+     * @return addIncomeResponse
+     */
+    public es.upm.fi.sos.upmbank.AddIncomeResponse addIncome(
+        es.upm.fi.sos.upmbank.AddIncome addIncome) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#addIncome");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param login
+     * @return loginResponse
+     */
+    public es.upm.fi.sos.upmbank.LoginResponse login(
+        es.upm.fi.sos.upmbank.Login login) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#login");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param getMyMovements
+     * @return getMyMovementsResponse
+     */
+    public es.upm.fi.sos.upmbank.GetMyMovementsResponse getMyMovements(
+        es.upm.fi.sos.upmbank.GetMyMovements getMyMovements) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#getMyMovements");
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @param changePassword
+     * @return changePasswordResponse
+     */
+    public es.upm.fi.sos.upmbank.ChangePasswordResponse changePassword(
+        es.upm.fi.sos.upmbank.ChangePassword changePassword) {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#changePassword");
+    }
+}
