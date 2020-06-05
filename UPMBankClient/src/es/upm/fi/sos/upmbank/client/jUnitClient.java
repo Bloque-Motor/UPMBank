f64defcb25c199b2886e114de0fda4c3cd492937
Saabertooth1 <nalas3@icloud.com>
2020-06-05 13:57:15 +0200

    Started jUnitClient

---
+++ UPMBankClient/src/es/upm/fi/sos/upmbank/client/jUnitClient.java
@@ -0,0 +1,121 @@
+package es.upm.fi.sos.upmbank.client;
+
+import es.upm.fi.sos.upmbank.client.UPMBankWSStub.*;
+import org.apache.axis2.AxisFault;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class jUnitClient {
+
+    private final UPMBankWSStub stub;
+    private User user;
+    private Random random;
+    private ArrayList<String> accounts;
+
+    public jUnitClient(String username, String password) throws AxisFault {
+        stub = new UPMBankWSStub();
+        this.user.setName(username);
+        this.user.setPwd(password);
+        this.random = new Random();
+        this.accounts = new ArrayList<>();
+    }
+
+    public jUnitClient(String username) throws AxisFault {
+        stub = new UPMBankWSStub();
+        this.user.setName(username);
+        this.random = new Random();
+        this.accounts = new ArrayList<>();
+    }
+
+    boolean login() throws RemoteException {
+        Login login = new Login();
+        login.setArgs0(user);
+        LoginResponse response = stub.login(login);
+        return response.get_return().getResponse();
+    }
+
+    void logout() throws RemoteException {
+        Logout logout = new Logout();
+        stub.logout(logout);
+    }
+
+    AddUserResponse addUser(String username) throws RemoteException {
+        Username var = new Username();
+        var.setUsername(username);
+        AddUser addUser = new AddUser();
+        addUser.setArgs0(var);
+        AddUserResponse response = stub.addUser(addUser).get_return();
+        if (response.getResponse()) {
+            this.user.setPwd(response.getPwd());
+        }
+        return response;
+    }
+
+    Response removeUser(String username) throws RemoteException {
+        Username var = new Username();
+        var.setUsername(username);
+        RemoveUser removeUser = new RemoveUser();
+        removeUser.setArgs0(var);
+        return stub.removeUser(removeUser).get_return();
+    }
+
+    Response changePassword(String newPwd) throws RemoteException {
+        PasswordPair var = new PasswordPair();
+        var.setNewpwd(newPwd);
+        var.setOldpwd(this.user.getPwd());
+        ChangePassword changePwd = new ChangePassword();
+        Response response = stub.changePassword(changePwd).get_return();
+        if (response.getResponse()) {
+            this.user.setPwd(newPwd);
+        }
+        return response;
+    }
+
+    BankAccountResponse addBankAcc(double qtty) throws RemoteException {
+        Deposit var = new Deposit();
+        var.setQuantity(qtty);
+        AddBankAcc addBankAcc = new AddBankAcc();
+        addBankAcc.setArgs0(var);
+        AddBankAccResponse addBankAccResponse = stub.addBankAcc(addBankAcc);
+        BankAccountResponse response = addBankAccResponse.get_return();
+        if (response.getResult()) {
+            accounts.add(response.getIBAN());
+        }
+        return response;
+    }
+
+    Response closeBankAcc(String iban) throws RemoteException {
+        BankAccount var = new BankAccount();
+        var.setIBAN(iban);
+        CloseBankAcc closeBankAcc = new CloseBankAcc();
+        closeBankAcc.setArgs0(var);
+        Response response = stub.closeBankAcc(closeBankAcc).get_return();
+        if (response.getResponse()) {
+            accounts.remove(iban);
+        }
+        return response;
+    }
+
+    AddMovementResponse addMovement(String iban, double qtty) throws RemoteException {
+        Movement var = new Movement();
+        var.setIBAN(iban);
+        var.setQuantity(qtty);
+        AddIncome addIncome = new AddIncome();
+        addIncome.setArgs0(var);
+        return stub.addIncome(addIncome).get_return();
+    }
+
+    AddMovementResponse addWithdrawal(String iban, double qtty) throws RemoteException {
+        Movement var = new Movement();
+        var.setIBAN(iban);
+        var.setQuantity(qtty);
+        AddWithdrawal addWithdrawal = new AddWithdrawal();
+
+    }
+
+
+
+
+}
--- UPMBankClient/test/Client1Test.java
+++ UPMBankClient/test/Client1Test.java
@@ -1,2 +1,18 @@
+import es.upm.fi.sos.upmbank.client.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Client1Test {
+
+    private Client client;
+
+    @BeforeAll
+    void loadClient() {client = new Client();}
+
+    @Test
+    @DisplayName("Sudo login")
+    void sudoLogin1() {
+
+    }
 }
